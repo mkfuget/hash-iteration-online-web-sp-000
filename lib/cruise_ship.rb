58eb99be51249cdec4061ec25670f94273a9625b
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 03:47:39 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,10 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      return name
+    end
+  end
+  return "Not Found"
 end
